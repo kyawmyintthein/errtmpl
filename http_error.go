95632c2e535f2afd21f2a4d0364616eacc2f652d
Kyaw Myint Thein <kyaw@traverse.ai>
2017-03-16 17:37:00 +0800

    Convert error to HttpError struct

--- http_error.go
+++ http_error.go
@@ -1,30 +1,38 @@
 package errtmpl
 
-import(
+import (
 	"net/http"
 	"strings"
 )
 
 const (
-	BadRequst = http.StatusBadRequest
-	Unauthorized = http.StatusUnauthorized
-	Forbidden = http.StatusForbidden
-	NotFound = http.StatusNotFound
+	BadRequst        = http.StatusBadRequest
+	Unauthorized     = http.StatusUnauthorized
+	Forbidden        = http.StatusForbidden
+	NotFound         = http.StatusNotFound
 	MethodNotAllowed = http.StatusMethodNotAllowed
 )
 
 type (
-	HttpError struct{
-		HttpStatus 					int              `json:"status"`
-		Code 						int              `json:"error_code"`
-		Message					string			 	 `json:"message"`
+	HttpError struct {
+		HttpStatus int    `json:"status"`
+		Code       int    `json:"error_code"`
+		Message    string `json:"message"`
 	}
 )
 
-func HttpError(e errorString, status int) Error{
+func ErrorWithHttpStatus(e errorString, status int) HttpError {
 	return HttpError{
-   		HttpStatus: status,
-   		Code: err.code,
-   		Message: err.message,
-   	}
+		HttpStatus: status,
+		Code:       err.code,
+		Message:    err.message,
+	}
+}
+
+func DefaultErrorWithHttpStatus(err error, code string, status int) HttpError {
+	return HttpError{
+		HttpStatus: status,
+		Code:       err.code,
+		Message:    err.message,
+	}
 }
